ff70d8a50ee3a7e11308a7d68dd0f8e090d64576
eduardo <eduloni3@gmail.com>
2020-02-04 16:23:26 +0000

    create and migrate model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+  title= models.CharField(max_length=250)
+  description = models.CharField(max_length=500)
+
+  def __str__(self):
+    return self.title
